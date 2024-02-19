9f34ab645f9d0cea32dae5987052403250def9f3
Joce <jocelyn.berenice17@gmail.com>
2024-02-19 13:26:07 -0600

    ci: funcion saludar apersona

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hola CH38")
+
+console.log("hola CH38");
+
+// FUncion que envie un saludo con alert.
+
+function saludo(){
+    alert('Holaaaa CH38');
+}
+
+/**
+ * Pedir el nombre de la persona usanso un promt()
+ * Mostrar un saludo y nombre de la persona con alert()
+ */
+
+function enviarsaludoApersona(){
+    let nombreUsuario = prompt("¿Cual es tu nombre?");
+    
+    alert("Hola " + nombreUsuario);
+}
